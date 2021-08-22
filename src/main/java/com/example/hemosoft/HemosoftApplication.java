9e092835a78f2e7dba75ff31161d433eee6d3ba5
mertsahin <mertsahin58@hotmail.com>
2021-08-22 13:42:07 +0300

    s

--- src/main/java/com/example/hemosoft/HemosoftApplication.java
+++ src/main/java/com/example/hemosoft/HemosoftApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-
 public class HemosoftApplication {
 
 	public static void main(String[] args) {
